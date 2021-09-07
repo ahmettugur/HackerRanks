6a35b6cb26157368191edc6ca3f34ee0c8387d25
Ahmet Tügür <ahmet_tgr@hotmail.com>
2021-09-07 16:56:36 +0300

    add countApplesAndOranges method

---
+++ HackerRanks/HackerRanks/CountApplesAndOranges.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRanks
+{
+    public class CountApplesAndOranges
+    {
+        public static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+        {
+            var appleCount = 0;
+            var orangeCount = 0;
+            int totalApplesCount = apples.Count;
+            int totalOrangesCount = oranges.Count;
+            int maxLength = Math.Max(totalApplesCount, totalOrangesCount);
+            int distance;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < totalApplesCount)
+                {
+                    distance = a + apples[i];
+                    if (distance >= s && distance <= t)
+                    {
+                        appleCount++;
+                    }
+                }
+                if (i < totalOrangesCount)
+                {
+                    distance = b + oranges[i];
+                    if (distance >= s && distance <= t)
+                    {
+                        orangeCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+        }
+    }
+}
--- HackerRanks/HackerRanks/Program.cs
+++ HackerRanks/HackerRanks/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
+using System.Collections.Generic;
+
 namespace HackerRanks
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> apples = new List<int> { -2, 2, 1 };
+            List<int> oranges = new List<int> { 5, -6 };
+            CountApplesAndOranges.countApplesAndOranges(7, 11, 5, 15, apples, oranges);
+            Console.ReadLine();
         }
     }
 }
